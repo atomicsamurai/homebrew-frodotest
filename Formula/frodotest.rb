2bf4c7025a022bb8217ace79559d4bf8cb694c61
Sandeep Chaturvedi <schaturvedi@gmail.com>
2023-02-26 12:24:27 -0700

    added formula

---
+++ Formula/frodotest.rb
@@ -0,0 +1,10 @@
+class Hello < Formula
+  desc "frodo hb test"
+  homepage "https://github.com/atomicsamurai/homebrew-frodotest"
+  url "https://github.com/atomicsamurai/homebrew-frodotest/releases/download/0.23.0/frodotest-macos-0.23.0.tar.gz"
+  sha256 "17596ac6dc2ebad120461636a7d988d1371a512ee92d7f9aa6fb6ad14648a046"
+  version "0.23.0"
+  def install
+    bin.install "frodotest"
+  end
+end
